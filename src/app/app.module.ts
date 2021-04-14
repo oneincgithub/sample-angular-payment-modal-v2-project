4c5b87bfb735025f8d0c74fa6894167522f2c8af
One Inc Github <39503219+oneincgithub@users.noreply.github.com>
2021-04-14 14:13:04 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { InstantPaymentComponent } from "./instant-payment.component";
 import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule],
   declarations: [AppComponent, InstantPaymentComponent],
   bootstrap: [AppComponent]
 })
